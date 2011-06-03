9c1f3204f8c654ab34eca3ebb519254032ddea33
Jason Gerard DeRose <jderose@novacut.com>
2011-06-03 14:30:38 -0600

    Started work on edit schema

---
+++ novacut/schema.py
@@ -0,0 +1,74 @@
+# novacut: the distributed video editor
+# Copyright (C) 2011 Novacut Inc
+#
+# This file is part of `novacut`.
+#
+# `novacut` is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# `novacut` is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+# more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with `novacut`.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#   Jason Gerard DeRose <jderose@novacut.com>
+
+"""
+Test-driven definition of novacut edit node schema.
+
+Example of edit node:
+
+
+>>> doc = {
+...     '_id': '3HHSRSVXT5ZGY2B6LJPN457P',
+...     'ver': 0,
+...     'type': 'novacut/node',
+...     'time': 1234567890,
+...     'node': {
+...         'src': 'VYUG4ON2APZK3GEJULB4I7PHJTKZLXTOIRGU2LU2LW7JBOCU',
+...         'type': 'slice',
+...         'start_frame': 123,
+...         'end_frame': 456,
+...     },
+... }
+
+
+Design Decision: hashable semantic content
+==========================================
+
+The CouchDB document corresponding to each node in the editing graph has two
+distinct purposes: unambiguously store the "meaning" of the edit semantically
+speaking, and to provide and extensible place to store various annotations.
+
+The semantic portion of of the edit is stored in a dictionary under the "node"
+key.  For example:
+
+>>> doc = {
+...     'node': {},
+... }
+
+The "node" dictionary only stores semantic information, the data that uniquely
+describes that exact node.  This is perhaps easier to explain in terms of what
+is *not* stored in "node".  For example, things like timestamps and annotations
+aren't stored in "node" because regardless of what time the node was created or
+last changed, the meaning of the edit is the same.
+
+This is done so that "node" can be serialized into a standard form and hashed,
+giving us a globally unique way to refer to any node in any editing graph.  This
+is enormously useful for two reasons:
+
+    1. It gives us an easy unique ID for caching pre-rendered corresponding to
+       a specific node (semantically speaking)
+
+    2.
+
+
+"""
+
+import json
