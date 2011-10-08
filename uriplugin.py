c54d1594c7612e8ae566f7f57c515e23d3877278
Jason Gerard DeRose <jderose@novacut.com>
2011-10-07 21:06:44 -0600

    Got property working

--- uriplugin.py
+++ uriplugin.py
@@ -1,3 +1,6 @@
+from microfiber import random_id
+from filestore import FileStore, DIGEST_BYTES
+
 from gi.repository import GObject, Gst
 
 GObject.threads_init()
@@ -21,15 +24,37 @@ class DmediaSrc(Gst.Bin):
         ),
     )
 
+    __gproperties__ = {
+        'id': (
+            GObject.TYPE_STRING,
+            'id',
+            'dmedia file ID',
+            None,
+            GObject.PARAM_READWRITE,
+        ),
+    }
+
     def __init__(self):
         super(DmediaSrc, self).__init__()
+        self._id = None
         self._filesrc = Gst.ElementFactory.make('filesrc', None)
+        self._fs = FileStore('/home')
         self.add(self._filesrc)
         self.add_pad(
             Gst.GhostPad.new('src', self._filesrc.get_pad('src'))
         )
 
- 
+    def do_get_property(self, name):
+        return self._id
+
+    def do_set_property(self, name, value):
+        self._id = value
+        if value is None:
+            self._filesrc.set_property('location', None)
+        else:
+            self._filesrc.set_property('location', self._fs.path(value))
+
+
 def plugin_init(plugin, userarg):
     DmediaSrcType = GObject.type_register(DmediaSrc)
     return Gst.Element.register(plugin, 'dmediasrc', 0, DmediaSrcType)
@@ -51,5 +76,7 @@ Gst.Plugin.register_static_full(
 )
 
 src = Gst.ElementFactory.make('dmediasrc', None)
-print(src)
-
+_id = random_id(DIGEST_BYTES)
+src.set_property('id', _id)
+print(src.get_property('id'))
+print(src._filesrc.get_property('location'))
