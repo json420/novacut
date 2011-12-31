8c729ee8715d0f0bd16cd0dd3b84ddfd669ba9cb
Jason Gerard DeRose <jderose@novacut.com>
2011-12-31 08:19:35 -0700

    Quick proof of concept with project list on projects page

--- ui/projects.js
+++ ui/projects.js
@@ -10,3 +10,33 @@ function open_project(project_id) {
 }
 
 Hub.connect('project_created', open_project);
+
+
+var UI = {
+    on_projects: function(req) {
+        var rows = req.read()['rows'];
+        var div = $('projects');
+        div.textContent = '';
+        rows.forEach(function(row) {
+            var _id = row.id;
+            var p = $el('p', {id: _id});
+            if (row.value) {
+                p.textContent = row.value;
+            }
+            else {
+                p.appendChild($el('em', {textContent: 'Untitled'}));
+            }
+            p.onclick = function() {
+                open_project(_id);
+            }
+            div.appendChild(p);
+        });
+    },
+}
+
+
+window.addEventListener('load',
+    function() {
+        novacut.view(UI.on_projects, 'project', 'atime', {descending: true});
+    }
+);
