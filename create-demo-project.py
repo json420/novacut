b91b19afe699a2c9655ecf72661f16316f18563f
Jason Gerard DeRose <jderose@novacut.com>
2012-01-19 18:27:46 -0700

    Added another static demo for testing

---
+++ create-demo-project.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import json
+import time
+from microfiber import dmedia_env, Database, Conflict
+
+docs = json.load(open('demo.json', 'r'))
+for doc in docs:
+    doc['ver'] = 0
+    doc['time'] = time.time()
+
+
+p = {
+    '_id': '5A236DNAC6NMS5XIBUMHTY2A',
+    'ver': 0,
+    'title': '2 Clip Demo',
+    'db_name': 'novacut-0-5a236dnac6nms5xibumhty2a',
+    'time': 1327022245.45872,
+    'atime': 1327022245.45872,
+    'type': 'novacut/project',
+}
+
+env = dmedia_env()
+db = Database('novacut-0', env)
+db.ensure()
+project = Database(p['db_name'], env)
+if project.ensure():
+    project.post(p)
+    project.bulksave(docs)
+try:
+    db.save(p)
+except Conflict:
+    pass
+
+
