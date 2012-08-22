a23ca5f1c223a9ea075b1033c9af70a4eeb0da86
Jason Gerard DeRose <jderose@novacut.com>
2012-08-22 02:55:35 -0600

    Added new mapper module

---
+++ novacut/tests/test_mapper.py
@@ -0,0 +1,58 @@
+# novacut: the collaborative video editor
+# Copyright (C) 2012 Novacut Inc
+#
+# This file is part of `novacut`.
+#
+# `novacut` is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# `novacut` is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+# more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with `novacut`.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#   Jason Gerard DeRose <jderose@novacut.com>
+
+"""
+Unit tests for the `novacut.mapper` module.
+"""
+
+from unittest import TestCase
+from fractions import Fraction
+
+from novacut import mapper
+
+
+class TestFunctions(TestCase):
+    def test_get_fraction(self):
+        self.assertEqual(
+            mapper.get_fraction({'num': 30000, 'denom': 1001}),
+            Fraction(30000, 1001)
+        )
+        self.assertEqual(
+            mapper.get_fraction([30000, 1001]),
+            Fraction(30000, 1001)
+        )
+        self.assertEqual(
+            mapper.get_fraction((30000, 1001)),
+            Fraction(30000, 1001)
+        )
+        self.assertEqual(
+            mapper.get_fraction((30000, 1001, True)),
+            Fraction(30000, 1001)
+        )
+        frac = Fraction(24, 1)
+        self.assertIs(mapper.get_fraction(frac), frac)
+        with self.assertRaises(TypeError) as cm:
+            mapper.get_fraction('24/1')
+        self.assertEqual(
+            str(cm.exception),
+            "invalid fraction type <class 'str'>: '24/1'"
+        )
+
