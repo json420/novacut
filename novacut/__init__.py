aa7a43543f20f1a71b359f90596d20c7a806bc5e
Jason Gerard DeRose <jderose@novacut.com>
2013-02-28 19:21:50 -0700

    Bumped version to 13.03

--- novacut/__init__.py
+++ novacut/__init__.py
@@ -23,7 +23,7 @@
 `novacut` - the collaborative video editor.
 """
 
-__version__ = '13.02.0'
+__version__ = '13.03.0'
 BUS = 'com.novacut.Renderer'
 
 
--- novacut2/__init__.py
+++ novacut2/__init__.py
@@ -26,7 +26,7 @@ This will go away once we can build our renderer with GStreamer 1.0, PyGI, and
 Python3.
 """
 
-__version__ = '13.02.0'
+__version__ = '13.03.0'
 
 
 def configure_logging():
