99909c1e61005cb6a50907b494591675461119f4
Jason Gerard DeRose <jason@system76.com>
2015-08-13 08:44:48 -0600

    Doodle on video encoder pipeline whose src element is an appsrc

--- novacut/renderer2.py
+++ novacut/renderer2.py
@@ -31,7 +31,8 @@ gi.require_version('Gst', '1.0')
 from gi.repository import GLib, Gst
 
 from .timefuncs import frame_to_nanosecond, nanosecond_to_frame
-from .misc import random_slice
+from .misc import random_slice, random
+from .renderer import make_element
 
 Gst.init(None)
 mainloop = GLib.MainLoop()
@@ -57,76 +58,26 @@ log = logging.getLogger()
 Info = namedtuple('Info', 'filename framerate start stop')
 
 
-class VideoSlice:
-    def __init__(self, info, helper):
-        assert info.start < info.stop
-        self.info = info
-        self.helper = helper
-        self.cur = info.start
+class Pipeline:
+    def __init__(self):
 
         self.pipeline = Gst.Pipeline()
         self.bus = self.pipeline.get_bus()
         self.bus.add_signal_watch()
-        self.bus.connect('message::segment-done', self.on_segment_done)
-        self.bus.connect('message::eos', self.on_eos)
         self.bus.connect('message::error', self.on_error)
 
-        # Create elements
-        self.src = Gst.ElementFactory.make('filesrc', None)
-        self.dec = Gst.ElementFactory.make('decodebin', None)
-        self.sink = Gst.ElementFactory.make('appsink', None)
-
-        # Add elements to pipeline
-        for element in (self.src, self.dec, self.sink):        
-            self.pipeline.add(element)
-
-        # Set properties
-        self.src.set_property('location', info.filename)
-        self.sink.set_property('emit-signals', True)
-
-        # Connect signal handlers
-        self.dec.connect('pad-added', self.on_pad_added)
-        self.dec.connect('no-more-pads', self.on_no_more_pads)
-        self.dec.connect('drained', self.on_drained)
-        self.sink.connect('new-preroll', self.on_new_preroll)
-        self.sink.connect('new-sample', self.on_new_sample)
-
-        # Link elements
-        self.src.link(self.dec)
+    def set_state(self, name, sync=False):
+        log.info('set_state(%r, sync=%r)', name, sync)
+        assert sync in (True, False)
+        state = getattr(Gst.State, name)
+        self.pipeline.set_state(state)
+        if sync is True:
+            self.pipeline.get_state(Gst.CLOCK_TIME_NONE)
 
     def destroy(self):
-        self.pipeline.set_state(Gst.State.NULL)
-
-    def run(self):
-        self.pipeline.set_state(Gst.State.PAUSED)
-        self.pipeline.get_state(Gst.CLOCK_TIME_NONE)
-        self.pipeline.seek(
-            1.0,  # rate
-            Gst.Format.TIME,        
-            Gst.SeekFlags.FLUSH | Gst.SeekFlags.SEGMENT | Gst.SeekFlags.ACCURATE,
-            Gst.SeekType.SET,
-            frame_to_nanosecond(self.info.start, self.info.framerate),
-            Gst.SeekType.SET,
-            frame_to_nanosecond(self.info.stop, self.info.framerate),
-        )
-        self.pipeline.set_state(Gst.State.PLAYING)
-
-    def on_pad_added(self, dec, pad):
-        caps = pad.get_current_caps()
-        string = caps.to_string()
-        #log.info('on_pad_added(): %s', string)
-        if string.startswith('video/'):
-            pad.link(self.sink.get_static_pad('sink'))
-
-    def on_no_more_pads(self, dec):
-        log.info('on_no_more_pads()')
-
-    def on_drained(self, dec):
-        log.info('on_drained()')
-
-    def on_new_preroll(self, sink):
-        log.info('on_new_preroll()')
-        return Gst.FlowReturn.OK
+        self.set_state('NULL')
+        del self.bus
+        del self.pipeline
 
     def _fatal(self):
         log.info('shuting down after fatal error')
@@ -137,13 +88,69 @@ class VideoSlice:
         log.error(msg, *args)
         GLib.idle_add(self._fatal)
 
+    def on_error(self, bus, msg):
+        self.fatal('on_error: %s', msg.parse_error())
+
+    def make_element(self, name, props=None):
+        element = make_element(name, props)
+        self.pipeline.add(element)
+        return element
+
+
+class VideoSlice(Pipeline):
+    def __init__(self, info, helper):
+        super().__init__()
+
+        assert info.start < info.stop
+        self.info = info
+        self.helper = helper
+        self.cur = info.start
+
+        # Create elements
+        self.src = self.make_element('filesrc', {'location': info.filename})
+        self.dec = self.make_element('decodebin')
+        self.sink = self.make_element('appsink',
+            {
+                'emit-signals': True,
+                #'max-buffers': 10,
+                #'sync': False,
+            }
+        )
+
+        # Connect signal handlers
+        self.dec.connect('pad-added', self.on_pad_added)
+        self.sink.connect('new-sample', self.on_new_sample)
+
+        # Link elements
+        self.src.link(self.dec)
+
+    def run(self):
+        self.set_state('PAUSED', sync=True)
+        self.pipeline.seek(
+            1.0,  # rate
+            Gst.Format.TIME,        
+            Gst.SeekFlags.FLUSH | Gst.SeekFlags.SEGMENT | Gst.SeekFlags.ACCURATE,
+            Gst.SeekType.SET,
+            frame_to_nanosecond(self.info.start, self.info.framerate),
+            Gst.SeekType.SET,
+            frame_to_nanosecond(self.info.stop, self.info.framerate),
+        )
+        self.set_state('PLAYING')
+
+    def on_pad_added(self, dec, pad):
+        caps = pad.get_current_caps()
+        string = caps.to_string()
+        log.info('on_pad_added(): %s', string)
+        if string.startswith('video/'):
+            pad.link(self.sink.get_static_pad('sink'))
+
     def on_new_sample(self, sink):
         buf = sink.get_property('last-sample').get_buffer()
         cur = nanosecond_to_frame(buf.pts, self.info.framerate)
-        data = buf.extract_dup(0, buf.get_size())
-        h = sha1(data).hexdigest()
+#        data = buf.extract_dup(0, buf.get_size())
+#        h = sha1(data).hexdigest()
         log.info('new-sample: %s [%s:%s] @%s',
-            h, self.info.start, self.info.stop, cur
+            'nope', self.info.start, self.info.stop, cur
         )
 
         if self.cur != cur:
@@ -153,12 +160,14 @@ class VideoSlice:
                 self.info.start, cur, self.info.stop
             )
 
-        if cur not in self.helper.hashes:
-            self.helper.hashes[cur] = h
-        elif self.helper.hashes[cur] != h:
-            self.fatal('hash mismatch: %s: %s != %s',
-                cur, self.helper.hashes[cur], h
-            )
+#        if cur not in self.helper.hashes:
+#            self.helper.hashes[cur] = h
+#        elif self.helper.hashes[cur] != h:
+#            self.fatal('hash mismatch: %s: %s != %s',
+#                cur, self.helper.hashes[cur], h
+#            )
+
+        self.helper.output.push(buf)
 
         self.cur += 1
         if self.cur == self.info.stop:
@@ -166,33 +175,85 @@ class VideoSlice:
             GLib.idle_add(self.helper.on_stop)
         return Gst.FlowReturn.OK
 
-    def on_segment_done(self, bus, msg):
-        log.info('on_segment_done')
+
+class Output(Pipeline):
+    def __init__(self, dst, framerate):
+        super().__init__()
+        self.bus.connect('message::eos', self.on_eos)
+        
+        self.framerate = framerate
+        self.i = 0
+        caps = Gst.caps_from_string('video/x-raw, format=(string)I420, width=(int)1920, height=(int)1080, interlace-mode=(string)progressive, pixel-aspect-ratio=(fraction)1/1, chroma-site=(string)mpeg2, colorimetry=(string)bt709, framerate=(fraction)30000/1001')
+
+        self.src = self.make_element('appsrc',
+            {
+                'caps': caps,
+                'emit-signals': False,
+                'format': 3,
+                #'max-bytes': 16 * 1024 * 1024,
+                #'block': True,
+            }
+        )
+        self.enc = self.make_element('x264enc',
+            {'bitrate': 8192, 'psy-tune': 5}
+        )
+        self.mux = self.make_element('matroskamux')
+        self.sink = self.make_element('filesink', {'location': dst})
+
+        self.src.link(self.enc)
+        self.enc.link(self.mux)
+        self.mux.link(self.sink)
+
+    def run(self):
+        self.set_state('PLAYING')
+
+    def push(self, buf):
+        ts = frame_to_nanosecond(self.i, self.framerate)
+        buf.pts = ts
+        buf.dts = ts
+        assert buf.pts == buf.dts == ts
+        log.info('push %s, %s', self.i, ts)
+        self.i += 1
+        self.src.emit('push-buffer', buf)
+
+    def done(self):
+        log.info('appsrc will emit EOS')
+        self.src.emit('end-of-stream')
 
     def on_eos(self, bus, msg):
-        log.info('on_eos()')
-
-    def on_error(self, bus, msg):
-        self.fatal('on_error: %s', msg.parse_error())
-
+        log.info('eos')
+        self.destroy()
+        mainloop.quit()
 
 
 class Helper:
-    def __init__(self, filename, framerate):
-        self.filename = filename
+    def __init__(self, sources, dst, framerate):
+        self.sources = sources
         self.framerate = framerate
         self.hashes = {}
         self.count = 0
+        self.output = Output(dst, framerate)
+        self.output.run()
 
     def next_slice(self):
-        (start, stop) = random_slice(107)
-        return Info(self.filename, self.framerate, start, stop)
+        if not self.sources:
+            return None
+        src = self.sources.pop(0)
+        (start, stop) = random_slice(500)
+        return Info(src, self.framerate, start, stop)
+
+    def done(self):
+        self.output.done()
 
     def start(self):
-        self.count += 1
-        log.info('**** slice %s ****', self.count)
-        self.slice = VideoSlice(self.next_slice(), self)
-        self.slice.run()
+        info = self.next_slice()
+        if info is None:
+            self.done()
+        else:
+            self.count += 1
+            log.info('**** slice %s ****', self.count)
+            self.slice = VideoSlice(info, self)
+            self.slice.run()
 
     def on_stop(self):
         log.info('Helper.on_stop()')
@@ -200,13 +261,19 @@ class Helper:
         del self.slice
         self.start()
 
-filename = path.join(path.dirname(path.dirname(path.abspath(__file__))), 'MVI_5751.MOV')
-framerate = Fraction(30000, 1001)
 
 
 if __name__ == '__main__':
+    tree = path.dirname(path.dirname(path.abspath(__file__)))
+    sources = []
+    for name in sorted(os.listdir(tree)):
+        if name.endswith('.MOV'):
+            sources.append(path.join(tree, name))
+    random.shuffle(sources)
+    dst = path.join(tree, 'test.mkv')
+    framerate = Fraction(30000, 1001)
 
-
-    test = Helper(filename, framerate)
+    test = Helper(sources, dst, framerate)
     test.start()
     mainloop.run()
+
